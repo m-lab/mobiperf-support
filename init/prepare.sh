cfa94e6baf82af5c513e52cd515ff974d2405607
salarcon215 <steph@stephalarcon.org>
2014-09-24 12:28:53 -0400

    Modified prepare.sh to reflect changes in dir in MobiPerf main project.

--- init/prepare.sh
+++ init/prepare.sh
@@ -16,12 +16,12 @@ if test -d $BUILD_DIR ; then
     rm -rf $BUILD_DIR/*
 fi
 
-pushd $SOURCE_DIR/MobiPerf/measurementserver
+pushd $SOURCE_DIR/MobiPerf/measurement_server
     ./compile.sh
 popd
 
 mkdir -p $BUILD_DIR/mobiperf
 cp -r $SOURCE_DIR/MobiPerf/measurementserver/mlab/*  $BUILD_DIR/mobiperf/
-install -D -m 0755 $SOURCE_DIR/MobiPerf/measurementserver/initialize.sh $BUILD_DIR/init/initialize.sh
-install -D -m 0755 $SOURCE_DIR/MobiPerf/measurementserver/start.sh $BUILD_DIR/init/start.sh
-install -D -m 0755 $SOURCE_DIR/MobiPerf/measurementserver/stop.sh $BUILD_DIR/init/stop.sh
+install -D -m 0755 $SOURCE_DIR/MobiPerf/measurement_server/initialize.sh $BUILD_DIR/init/initialize.sh
+install -D -m 0755 $SOURCE_DIR/MobiPerf/measurement_server/start.sh $BUILD_DIR/init/start.sh
+install -D -m 0755 $SOURCE_DIR/MobiPerf/measurement_server/stop.sh $BUILD_DIR/init/stop.sh
